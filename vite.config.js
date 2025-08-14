7cfac9594d3a116f57da8c47de512a45987e2cbd
Piyush Tamoli <piyushmodi812@gmail.com>
2025-08-14 23:50:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TNP-website/', // <-- Add this line (your GitHub repo name)
   plugins: [react()],
 })
