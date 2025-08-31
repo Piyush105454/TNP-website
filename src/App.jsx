4f6ace84186958d0eab0e3b7bbd3261f311b0c2e
Piyush Tamoli <piyushmodi812@gmail.com>
2025-08-31 22:15:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import LoginWindow from "./components/LoginWindow";
-import SignUp from "./components/Signup"; // Assuming you have a SignUp component
-import Dashboard from "./components/Dashboard";
-import SignInPage from "./components/signin";
+import Home from "./pages/test";
+// import LoginWindow from "./components/LoginWindow";
+// import SignUp from "./components/Signup"; // Assuming you have a SignUp component
+// import Dashboard from "./components/Dashboard";
+// import SignInPage from "./components/signin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginWindow />} />
+       <Route path="/" element={<Home />} />
+{/*         <Route path="/login" element={<LoginWindow />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signin" element={<SignInPage />} />  */}
 
       </Routes>
     </Router>
